8027b5ee34d096252eefa4dffde74c2ba5ed7168
lyhoangvinh <vinhhl@paxcreation.com>
2020-10-08 13:49:12 +0700

    no message

--- app/src/main/java/com/lyhoangvinh/simple/ui/features/MainActivity.kt
+++ app/src/main/java/com/lyhoangvinh/simple/ui/features/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lyhoangvinh.simple.ui.features
 
 import androidx.activity.viewModels
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import com.lyhoangvinh.simple.R
 import com.lyhoangvinh.simple.databinding.ActivityMainBinding
 import com.lyhoangvinh.simple.ui.base.activity.BaseActivity
@@ -11,4 +13,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseViewModelActivity<ActivityMainBinding, MainViewModel>() {
     override fun getLayoutResource() = R.layout.activity_main
     override val viewModel: MainViewModel by viewModels()
+    override fun onBackPressed() {
+        if (!findNavController(R.id.navHostFragment).popBackStack()) {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 }
