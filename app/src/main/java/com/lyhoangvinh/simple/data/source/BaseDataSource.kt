e4cf35a72b5810316d55b6129631fc772a032fdf
lyhoangvinh <vinh270795@gmail.com>
2020-10-18 11:21:14 +0700

    no message

--- app/src/main/java/com/lyhoangvinh/simple/data/source/BaseDataSource.kt
+++ app/src/main/java/com/lyhoangvinh/simple/data/source/BaseDataSource.kt
@@ -1,8 +1,12 @@
 package com.lyhoangvinh.simple.data.source
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
 import com.lyhoangvinh.simple.data.entities.Resource
 import com.lyhoangvinh.simple.data.entities.Status
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.Response
@@ -29,15 +33,38 @@ abstract class BaseDataSource {
     }
 
     private fun <T> error(message: String): Resource<T> {
-        Log.e("BaseDataSource ",message)
+        Log.e("BaseDataSource ", message)
         return Resource.error("Network call has failed for a following reason: $message")
     }
+
+    /**
+     * The database serves as the single source of truth.
+     * Therefore UI can receive data updates from database only.
+     * Function notify UI about:
+     * [Status.SUCCESS] - with data from database
+     * [Status.ERROR] - if error has occurred from any source
+     * [Status.LOADING]
+     */
 
     protected suspend fun <T> resultFlow(call: suspend () -> Response<T>): Flow<Resource<T>> =
         flow {
             emit(Resource.loading())
             val responseStatus = getResource { call.invoke() }
             if (responseStatus.status == Status.SUCCESS) {
+                emit(responseStatus)
+            } else if (responseStatus.status == Status.ERROR) {
+                emit(Resource.error(responseStatus.message.orEmpty()))
+            }
+        }
+
+    protected suspend fun <T> resultFlow(call: suspend () -> Response<T>,
+                                         saveCallResult: suspend (T?) -> Unit)
+            : Flow<Resource<T>> =
+        flow {
+            emit(Resource.loading())
+            val responseStatus = getResource { call.invoke() }
+            if (responseStatus.status == Status.SUCCESS) {
+                saveCallResult(responseStatus.data)
                 emit(responseStatus)
             } else if (responseStatus.status == Status.ERROR) {
                 emit(Resource.error(responseStatus.message.orEmpty()))
