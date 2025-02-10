697395b63a036bf39791cad21150b247b6be6abe
Hoàng Vinh Lý <vinh270795@gmail.com>
2025-02-10 11:52:58 +0700

    Update AutoClearedValue.kt

--- app/src/main/java/com/lyhoangvinh/simple/utils/AutoClearedValue.kt
+++ app/src/main/java/com/lyhoangvinh/simple/utils/AutoClearedValue.kt
@@ -1,26 +1,28 @@
 package com.lyhoangvinh.simple.utils
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import java.lang.ref.WeakReference
 
 /**
  * A value holder that automatically clears the reference if the Fragment's view is destroyed.
  * @param <T>
 </T> */
-class AutoClearedValue<T>(fragment: Fragment, private var value: T?) {
+class AutoClearedValue<T : Any>(fragment: Fragment, value: T) {
+
+    private var valueRef: WeakReference<T>? = WeakReference(value)
+
     init {
-        val fragmentManager = fragment.fragmentManager
-        fragmentManager!!.registerFragmentLifecycleCallbacks(
-            object : FragmentManager.FragmentLifecycleCallbacks() {
-                override fun onFragmentViewDestroyed(fm: FragmentManager, f: Fragment) {
-                    this@AutoClearedValue.value = null
-                    fragmentManager.unregisterFragmentLifecycleCallbacks(this)
+        fragment.viewLifecycleOwnerLiveData.observe(fragment) { owner ->
+            owner?.lifecycle?.addObserver(object : DefaultLifecycleObserver {
+                override fun onDestroy(owner: LifecycleOwner) {
+                    valueRef?.clear()
+                    valueRef = null
                 }
-            }, false
-        )
+            })
+        }
     }
 
-    fun get(): T? {
-        return value
-    }
+    fun get(): T? = valueRef?.get()
 }
